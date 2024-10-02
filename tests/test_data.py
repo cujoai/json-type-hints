2f7c80bbcf536b2683c1502d20078215ecb5ab6b
Darius Sirvidas <darius.sirvidas@cujo.com>
2024-10-02 19:17:34 +0300

    update tests to cover objects encoded with no '__data__'

--- tests/test_data.py
+++ tests/test_data.py
@@ -13,21 +13,29 @@ class MyClass2:
     prop2: str
 
 
+@dataclass
+class MyClass3:
+    prop1: int = 1
+
+
 CLS_NAME_1 = "MyClass1"
 CLS_NAME_2 = "MyClass2"
+CLS_NAME_3 = "MyClass3"
 TYPE_DICT_CLS1 = {CLS_NAME_1: MyClass1}
 TYPE_DICT_CLS2 = {CLS_NAME_2: MyClass2}
-TYPE_DICT_FULL = {CLS_NAME_1: MyClass1, CLS_NAME_2: MyClass2}
+TYPE_DICT_PARTIAL = {CLS_NAME_1: MyClass1, CLS_NAME_2: MyClass2, CLS_NAME_3: MyClass3}
+TYPE_DICT_FULL = {CLS_NAME_1: MyClass1, CLS_NAME_2: MyClass2, CLS_NAME_3: MyClass3}
 CLASS_1_OBJ_1 = MyClass1(prop1=1, prop2=("bytes1", b"\x01"))
 CLASS_1_OBJ_2 = MyClass1(prop1=2, prop2=("bytes2", b"\x01\x00"))
-CLASS_2_OBJ_1 = MyClass2(prop1=[CLASS_1_OBJ_1, CLASS_1_OBJ_2], prop2="cls2")
+CLASS_3_OBJ_1 = MyClass3()
+CLASS_2_OBJ_1 = MyClass2(prop1=[CLASS_1_OBJ_1, CLASS_1_OBJ_2, CLASS_3_OBJ_1], prop2="cls2")
 INPUT_DICT = {"key1": 1, "key2": CLASS_2_OBJ_1}
 DUMPS_STR = (
     '{"key1":1,"key2":{"__type__":"MyClass2","__data__":{"prop1":[{"__type__":"MyClass1",'
     '"__data__":{"prop1":1,"prop2":{"__type__":"tuple","__data__":["bytes1",'
     '{"__type__":"bytes","__data__":"AQ=="}]}}},{"__type__":"MyClass1",'
     '"__data__":{"prop1":2,"prop2":{"__type__":"tuple","__data__":["bytes2",'
-    '{"__type__":"bytes","__data__":"AQA="}]}}}],"prop2":"cls2"}}}'
+    '{"__type__":"bytes","__data__":"AQA="}]}}},{"__type__":"MyClass3"}],"prop2":"cls2"}}}'
 )
 LOADS_DEFAULT_ONLY = {
     "key1": 1,
@@ -40,6 +48,7 @@ LOADS_DEFAULT_ONLY = {
                     "__type__": CLS_NAME_1,
                     "__data__": {"prop1": 2, "prop2": ("bytes2", b"\x01\x00")},
                 },
+                {"__type__": CLS_NAME_3},
             ],
             "prop2": "cls2",
         },
@@ -50,7 +59,7 @@ LOADS_CLS_1_ONLY = {
     "key2": {
         "__type__": CLS_NAME_2,
         "__data__": {
-            "prop1": [CLASS_1_OBJ_1, CLASS_1_OBJ_2],
+            "prop1": [CLASS_1_OBJ_1, CLASS_1_OBJ_2, {"__type__": CLS_NAME_3}],
             "prop2": "cls2",
         },
     },
--- tests/tests.py
+++ tests/tests.py
@@ -3,20 +3,23 @@ import unittest
 from test_data import (
     CLS_NAME_1,
     CLS_NAME_2,
+    CLS_NAME_3,
     DUMPS_STR,
     INPUT_DICT,
     LOADS_CLS_1_ONLY,
     LOADS_DEFAULT_ONLY,
     LOADS_STR_BAD_PROP,
     TYPE_DICT_CLS1,
-    TYPE_DICT_CLS2,
     TYPE_DICT_FULL,
+    TYPE_DICT_PARTIAL,
 )
 
 from src.json_hints import dumps, loads
 
 
 def encode_classes(obj):
+    if isinstance(obj, TYPE_DICT_FULL[CLS_NAME_3]):
+        return {"__type__": CLS_NAME_3}
     if type(obj) in TYPE_DICT_FULL.values():
         return {"__type__": type(obj).__name__, "__data__": obj.__dict__}
     return obj
@@ -30,7 +33,7 @@ def encode_classes_incomplete(obj):
 
 def decode_classes(obj):
     if "__type__" in obj and (_type := obj["__type__"]) in TYPE_DICT_FULL:
-        return TYPE_DICT_FULL[_type](**obj["__data__"])
+        return TYPE_DICT_FULL[_type](**obj.get("__data__", {}))
     return obj
 
 
@@ -67,11 +70,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_loads_no_parsers_no_raise(self):
         self.assertEqual(loads(DUMPS_STR, raise_on_unknown=False), LOADS_DEFAULT_ONLY)
-
-    def test_loads_no_data(self):
-        with self.assertRaises(TypeError) as cm:
-            loads('{"__type__":"tuple"}')
-        self.assert_exception_msg(cm.exception, "no '__data__'")
 
     def test_loads_bad_data(self):
         with self.assertRaises(TypeError) as cm:
@@ -111,7 +109,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_loads_dict_and_hook(self):
         self.assertEqual(
-            loads(DUMPS_STR, hinted_types=TYPE_DICT_CLS2, object_hook=decode_class1), INPUT_DICT
+            loads(DUMPS_STR, hinted_types=TYPE_DICT_PARTIAL, object_hook=decode_class1), INPUT_DICT
         )
 
     def test_dumps_and_loads(self):
